95872edeb1cb6d22846af952101098b4ec1f180d
George Fincher <george@grimfunky.dev>
2023-11-18 23:30:55 +0000

    Feat ✨: complete async api calls with go routines

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"frontendmasters.com/go/crypto/api"
 )
 
+// Main go routine
 func main() {
-	rate, err := api.GetRate("BTC")
+	currencies := []string {"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1)
+
+		go func (currencyCode string) {
+			getCurrencyData(currencyCode)
+			wg.Done()
+		}(currency)
+
+	}
+
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	rate, err := api.GetRate(currency)
 
 	if err == nil {
-		fmt.Printf("The rate for %v is %.2f", rate.Currency, rate.Price)
+		fmt.Printf("The rate for %v is %.2f \n", rate.Currency, rate.Price)
 	}
+
 }
