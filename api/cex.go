fa0625ef6c761e98f51777bdb8bfa7ea051f3202
George Fincher <george@grimfunky.dev>
2023-11-18 22:37:19 +0000

    Feat ✨: Complete error handling for CEX getCurrency request

--- api/cex.go
+++ api/cex.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
@@ -10,8 +11,29 @@ import (
 
 const apiUrl = "https://cex.io/api/ticker/%s/USD"
 
-func GetRate(currency string) (model.Rate, error) {
+func GetRate(currency string) (*model.Rate, error) {
 	upCurrency := strings.ToUpper(currency)
 
 	res, err := http.Get(fmt.Sprintf(apiUrl, upCurrency))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode == http.StatusOK {
+		bodyBytes, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			return nil, err
+		}
+
+		json := string(bodyBytes)
+
+		fmt.Println(json)
+	} else {
+		return nil, fmt.Errorf("status code received: %v", res.StatusCode)
+	}
+
+	rate := model.Rate{Currency: currency, Price: 20}
+	return &rate, nil
 }
