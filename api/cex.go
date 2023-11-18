5093f2754d9ce3776c52183b5b2618383785bd52
George Fincher <george@grimfunky.dev>
2023-11-18 22:22:45 +0000

    Feat ✨: Complete strings and first http request

--- api/cex.go
+++ api/cex.go
@@ -1,7 +1,17 @@
 package api
 
-import "frontendmasters.com/go/crypto/model"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"frontendmasters.com/go/crypto/model"
+)
+
+const apiUrl = "https://cex.io/api/ticker/%s/USD"
 
 func GetRate(currency string) (model.Rate, error) {
-	http
+	upCurrency := strings.ToUpper(currency)
+
+	res, err := http.Get(fmt.Sprintf(apiUrl, upCurrency))
 }
